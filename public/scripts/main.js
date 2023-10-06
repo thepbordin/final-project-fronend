d95fc09288d5172aa3429f342d470c0870b696f4
Thepbordin Jaiinsom <thepbordinjaiinsom@gmail.com>
2023-10-06 23:40:19 +0700

    form draft

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,8 @@
 import { renderRooms } from "./rooms.js";
 import { renderPosts } from "./posts.js";
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM ready");
     renderRooms();
@@ -11,4 +13,24 @@ const sortSelect = document.querySelector("#sort-select");
 sortSelect.addEventListener("change", () => {
     const roomid = document.querySelector(".add_button").getAttribute("roomid");
     renderPosts(roomid, sortSelect.value);
+});
+
+var addPage = true;
+const addPostButton = document.querySelector(".add_button");
+addPostButton.addEventListener("click", () => {
+    const postForm = document.querySelector(".post-form");
+    const roomList = document.querySelector(".subject-scroll");
+    const roomTitle = document.querySelector("#form-title");
+    if(addPage) {
+        postForm.style.display = "block";
+        roomList.style.display = "none";
+        roomTitle.innerHTML = "Share you note!";
+    }else{
+        postForm.style.display = "none";
+        roomList.style.display = "block";
+        roomTitle.innerHTML = "Select Room";
+    }
+
+    addPage = (addPage ? false : true);
+    
 });
